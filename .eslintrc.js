e39523a3a1944075169b5d0d0db1a01e654d3e8d
Yoshi Odei <yoshiodei@yahoo.com>
2023-02-28 14:33:35 -0800

    add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "react-app",
+    "react-app/jest",
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
+
